c4e1f58737f169b156dfe4970646b31148237ffc
GrapeJuicer <Dev.Grape@outlook.jp>
2024-12-26 02:54:22 +0900

    [update #1] Add Asciimage class and OpenCvSharp package
    `Asciimage.cs`に`Asciimage`クラスと`Generate`メソッドを追加し、`OpenCvSharp`名前空間を`Cv`として使用するための`using`ステートメントを追加しました。また、`Asciimage.csproj`に`OpenCvSharp4`パッケージのバージョン`4.10.0.20241108`を追加しました。

---
+++ Asciimage/Asciimage.cs
@@ -0,0 +1,15 @@
+﻿using Cv = OpenCvSharp;
+
+namespace Asciimage
+{
+    public static class Asciimage
+    {
+        public static AsciiMat Generate(Cv.Mat mat, AsciimageConfig config)
+        {
+            
+
+            // 仮の戻り値
+            return new AsciiMat(new char[0, 0]);
+        }
+    }
+}
