146dcf64bc5b57200111a60e4a5e97cd8c59c7c5
GrapeJuicer <Dev.Grape@outlook.jp>
2025-01-18 02:12:38 +0900

    [update #1] Update Test1.cs and add Test2.cs
    Test1.cs:
    - `OpenCvSharp` のインポートを削除し、`Asciimage.Brushes` と `SkiaSharp` のインポートを追加
    - `TestMethod1` メソッドの内容を大幅に変更し、`SKFont` オブジェクトの作成、文字列リストの生成、`SegmentCount` オブジェクトの作成、`CusomFontBrush` オブジェクトの作成、`SKBitmap` と `SKCanvas` の作成、画像のエンコードと保存を追加
    - `DrawDepth` メソッドを新たに追加し、`SKCanvas` に対して矩形を描画する処理を実装
    
    Test2.cs:
    - 新たに `Asciimage`、`Asciimage.Brushes`、`SkiaSharp`、および `System.Diagnostics` のインポートを追加
    - `Test2` クラスとその中の `TestMethod1` メソッドを新たに追加し、`SKFont` オブジェクトの作成、文字列リストの生成、`CusomFontBrush` オブジェクトの作成、画像ファイルの読み込み、`Asciimage` の生成、デバッグ情報の出力を実装

--- AsciimageTester/Test1.cs
+++ AsciimageTester/Test1.cs
@@ -1,4 +1,6 @@
-﻿using OpenCvSharp;
+﻿using Asciimage.Brushes;
+using OpenCvSharp;
+using SkiaSharp;
 using System.Diagnostics;
 
 namespace AsciimageTester
@@ -9,19 +11,60 @@ namespace AsciimageTester
         [TestMethod]
         public void TestMethod1()
         {
-            // print current directory
-            Console.WriteLine(Directory.GetCurrentDirectory());
+            SKFont font = new()
+            {
+                Typeface = SKTypeface.FromFamilyName("Cascadia Code"),
+                Size = 12,
+            };
 
-            Debug.WriteLine("This is debug");
-            // start.pngを読み込む
-            Mat img = Cv2.ImRead("star.png", ImreadModes.Color);
+            List<string> ss = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".Select(x => x.ToString()).ToList();
+            SegmentCount seg = new(14, 8);
+            List<SegmentCount> segs = [SegmentCount.OneByOne, SegmentCount.TwoByTwo, SegmentCount.FourByTwo, SegmentCount.FourByFour, seg];
 
-            Console.WriteLine(img.Width + " " + img.Height);
+            CusomFontBrush brush = new(font, ss, segs);
 
-            Asciimage.AsciimageConfig config = new(200, 50, colorMode: Asciimage.ColorMode.Binary);
+            int pointSize = 10;
+            int LineCount = 10;
+            int margin = 50;
 
-            var am = Asciimage.Asciimage.Generate(img, config);
-            Console.WriteLine(am.ToString());
+            var areaMap = brush.CharacterAreas[seg];
+
+            int Bw = LineCount * (pointSize * areaMap.SegmentSize.Horizontal + margin) + margin;
+            int Bh = (areaMap.RelativeAreaMap.Count / LineCount) * (pointSize * areaMap.SegmentSize.Vertical + margin) + margin;
+
+            var bitmap = new SKBitmap(Bw, Bh);
+            var canvas = new SKCanvas(bitmap);
+
+            DrawDepth(canvas, areaMap, pointSize, LineCount, margin);
+
+            using var image = SKImage.FromBitmap(bitmap);
+            using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+            using var stream = File.OpenWrite(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"Desktop\asciimage-test1.png"));
+            data.SaveTo(stream);
+        }
+
+        public static void DrawDepth(SKCanvas canvas, CharacterAreaMap areaMap, int pointSize = 10, int LineCount = 10, int margin = 50)
+        {
+            canvas.Clear(SKColors.Blue);
+
+            var keys = areaMap.RelativeAreaMap.Keys.ToArray();
+
+            for (int i = 0; i < areaMap.RelativeAreaMap.Count; i++)
+            {
+                for (int y = 0; y < areaMap.SegmentSize.Vertical; y++)
+                {
+                    for (int x = 0; x < areaMap.SegmentSize.Horizontal; x++)
+                    {
+                        canvas.DrawRect(
+                        margin + (i % LineCount) * (areaMap.SegmentSize.Horizontal * pointSize + margin)/*(0,0)までのOffset)*/ + x * pointSize,
+                        margin + (i / LineCount) * (areaMap.SegmentSize.Vertical * pointSize + margin)/*(0,0)までのOffset)*/ + y * pointSize,
+                        pointSize,
+                        pointSize,
+                        new() { Color = SKColor.FromHsv(0, 0, float.Round((float)(100 * areaMap.RelativeAreaMap[keys[i]][y, x]))) }
+                        );
+                    }
+                }
+            }
         }
     }
 }
